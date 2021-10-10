cf36bcbf9e155903bb8c59e2b98c7241e6e4df06
Andreas Cord-Landwehr <cordlandwehr@kde.org>
2021-10-10 09:34:23 +0200

    Update plasma-release script for tarball builds

--- scripts/plasma-release.sh
+++ scripts/plasma-release.sh
@@ -3,13 +3,13 @@
 # add/remove Plasma release recipes
 #
 # SPDX-FileCopyrightText: 2018-2019 Volker Krause <vkrause@kde.org>
-# SPDX-FileCopyrightText: 2020 Andreas Cord-Landwehr <cordlandwehr@kde.org>
+# SPDX-FileCopyrightText: 2020-2021 Andreas Cord-Landwehr <cordlandwehr@kde.org>
 #
 # SPDX-License-Identifier: MIT
 
 function usage()
 {
-    echo "$1 [add|remove|update] <version> [<new version>]"
+    echo "$1 [add|add-tarball|remove|update <version> [<new version>]"
     exit 1
 }
 
@@ -25,14 +25,40 @@ case $command in
 add)
     for recipe in `find $base -name "*.inc" | grep -v /staging/`; do
         name=`echo $recipe | sed -e "s,\.inc,_${version}.bb,"`
+        app=$(echo $recipe | grep -P -o '[0-9a-zA-Z\-]+(?=\.inc)')
 cat <<EOM > $name
 # SPDX-FileCopyrightText: none
 # SPDX-License-Identifier: CC0-1.0
 
 require \${PN}.inc
+SRC_URI = "git://anongit.kde.org/${app};nobranch=1;protocol=https"
 SRCREV = "v\${PV}"
+S = "\${WORKDIR}/git"
 EOM
         git add $name
+    done
+    ;;
+add-tarball)
+    # search for all non-staging inc files without underlines
+    for recipe in $(find $base -regex ".*/[0-9a-zA-Z\-]+\.inc" | grep -v /staging/); do
+        name=$(echo $recipe | sed -e "s,\.inc,_${version}.bb,")
+        app=$(echo $recipe | grep -P -o '[0-9a-zA-Z\-]+(?=\.inc)')
+        url="https://download.kde.org/stable/plasma/${version}/${app}-${version}.tar.xz"
+        sha256=$(curl -s "${url}.sha256" | cut -d" " -f1)
+        echo "${url} : ${sha256}"
+# examples:
+#https://download.kde.org/stable/plasma/5.22.5/bluedevil-5.22.5.tar.xz
+#https://download.kde.org/stable/plasma/5.22.5/bluedevil-5.22.5.tar.xz.sha256
+cat <<EOM > $name
+# SPDX-FileCopyrightText: none
+# SPDX-License-Identifier: CC0-1.0
+
+require \${PN}.inc
+SRC_URI = "${url}"
+SRC_URI[sha256sum] = "${sha256}"
+${extraconfig}
+EOM
+         git add $name
     done
     ;;
 remove)
